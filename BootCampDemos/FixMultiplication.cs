284df749a1e25e2d22a23bf403b2a939b03d6ace
ahegana <ahegana@affinionds.com>
2019-06-25 23:22:05 +0530

    First version without refactoring, All tests are passing

---
+++ BootCampDemos/FixMultiplication.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCampDemos
+{
+    internal class FixMultiplication
+    {
+        public int FindDigit(string equation)
+        {
+            string[] tokens = equation.Split('=', '*');
+            if (tokens.Length != 3)
+                throw new ArgumentException("Invalid argument");
+
+            decimal[] numbers = new decimal[3];
+            string questionMarkContainingToken = string.Empty;
+            int questionMarkContainingTokenIndex = -1;
+            for (int i = 0; i < 3; i++)
+            {
+                bool hasQuestionMark = tokens[i].Contains("?");
+                if (hasQuestionMark)
+                {
+                    numbers[i] = -1;
+                    questionMarkContainingToken = tokens[i];
+                    questionMarkContainingTokenIndex = i;
+                }
+                else
+                {
+                    numbers[i] = decimal.Parse(tokens[i]);
+                }
+            }
+            string result = string.Empty;
+            decimal actualResult = CalculateResult(numbers, questionMarkContainingTokenIndex);
+
+            int indexofQuestionMark = questionMarkContainingToken.IndexOf("?");
+            int myMissingDigit = CalculateMissingdigit(questionMarkContainingToken, actualResult, indexofQuestionMark);
+            return myMissingDigit;
+        }
+
+        private int CalculateMissingdigit(string questionMarkContainingToken, decimal actualResult, int indexofQuestionMark)
+        {
+            int missingDigit = -1;
+            char missingCharacter = actualResult.ToString()[indexofQuestionMark];
+            questionMarkContainingToken = questionMarkContainingToken.Replace('?', missingCharacter);
+
+            if (questionMarkContainingToken == actualResult.ToString())
+            {
+                missingDigit = int.Parse(missingCharacter.ToString());
+            }
+
+            return missingDigit;
+        }
+
+        private decimal CalculateResult(decimal[] numbers, int questionMarkContainingTokenIndex)
+        {
+            decimal actualResult = -1;
+            if (questionMarkContainingTokenIndex == 0)
+            {
+                actualResult = numbers[2] / numbers[1];
+            }
+            else if (questionMarkContainingTokenIndex == 1)
+            {
+                actualResult = numbers[2] / numbers[0];
+            }
+            else if (questionMarkContainingTokenIndex == 2)
+            {
+                actualResult = numbers[0] * numbers[1];
+            }
+
+            return actualResult;
+        }
+    }
+}
--- BootCampDemos/Program.cs
+++ BootCampDemos/Program.cs
@@ -6,7 +6,26 @@ namespace BootCampDemos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Test("42*47=1?74", 9);
+            Test("4?*47=1974", 2);
+            Test("42*?7=1974", 4);
+            Test("42*?47=1974", -1);
+            Test("2*12?=247", -1);
+            Console.ReadKey(true);
+        }
+
+        private static void Test(string args, int expected)
+        {
+            var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
+            Console.WriteLine($"{args} : {result}");
+        }
+
+        public static int FindDigit(string equation)
+        {
+            FixMultiplication fixMultiplication = new FixMultiplication();
+            int result = fixMultiplication.FindDigit(equation);
+
+            return result;
         }
     }
-}
+    }
